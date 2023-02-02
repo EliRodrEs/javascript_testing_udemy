535f6eb155d61ebfb3d4dd7fcf2f4916a2b365e6
EliRodrEs <60432604+EliRodrEs@users.noreply.github.com>
2023-02-02 15:56:17 +0100

    Add test for promises

--- 03-async-code-starting-project/async/async-example.test.js
+++ 03-async-code-starting-project/async/async-example.test.js
@@ -1,16 +1,45 @@
 import { describe, it, expect } from 'vitest'
-import { generateToken } from './async-example'
+import { generateToken, generateTokenPromise } from './async-example'
 
-it('should generate a token value', (done) => {
-  const testUserEmail = 'test@test.com'
-
-  generateToken(testUserEmail, (err, token) => {
-    try {
-      expect(token).toBeDefined()
-      //expect(token).toBe(2)
-      done()
-    } catch (error) { //WE NEED TRY/CATCH TO ACTUALLY GET THE REAL ERRORS FROM ASYNC CODE
-      done(error)
-    }
+describe('generateToken()', () => {
+  it('should generate a token value', (done) => {
+    const testUserEmail = 'test@test.com'
+    generateToken(testUserEmail, (err, token) => {
+      try {
+        expect(token).toBeDefined()
+        //expect(token).toBe(2)
+        done()
+      } catch (error) { //WE NEED TRY/CATCH TO ACTUALLY GET THE REAL ERRORS FROM ASYNC CODE
+        done(error)
+      }
+    })
   })
+})
+
+describe('generateTokenPromise()', () => {
+  it('should generate a token value', () => {
+    const testUserEmail = 'test@test.com'
+		//expect supports promises out of the box <3<3 (resolves or rejects)
+		return expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined() 
+  })
+
+	//You can also use async/await if you need to check multiple steps.
+	it('should generate a token value', async () => {
+    const testUserEmail = 'test@test.com'
+		
+		const token = await generateTokenPromise(testUserEmail)
+
+		expect(token).toBeDefined()
+  })
+
+/* 	Even though the test worked as expected in the previous lecture, you should actually return the promise assertion in your tests:
+
+	it('should generate a token value', () => {
+		const testUserEmail = 'test@test.com'
+		return expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined()
+	})
+
+	This guarantees that Vitest / Jest wait for the promise to be resolved.
+	You don't need to return when using async / await (since a function annotated with async returns a promise implicitly). 
+*/
 })
