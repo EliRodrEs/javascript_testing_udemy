7ad56f043f22edaefe8d4577a19870d8d9c1c76d
EliRodrEs <60432604+EliRodrEs@users.noreply.github.com>
2023-02-04 13:48:27 +0100

    Add mockImplementation documentation

--- 01-starting-project/src/data.test.js
+++ 01-starting-project/src/data.test.js
@@ -6,6 +6,10 @@ describe('generateReportData()', () => {
   it('should execute logFn if provided', () => {
     const logger = vi.fn()
 
+    logger.mockImplementation(() => {
+      
+    }) //This overrides the global mock just for this file!! [mockImplementationOnce() does the same but just for the first time a method is called]
+
     generateReportData(logger)
 
     expect(logger).toBeCalled()
