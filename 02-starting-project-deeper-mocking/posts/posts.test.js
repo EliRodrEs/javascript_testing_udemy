d0b42ba9211dcd6a7b67113cb1f8b3295ed7716d
EliRodrEs <60432604+EliRodrEs@users.noreply.github.com>
2023-02-06 16:29:44 +0100

    Add posts.test.js

---
+++ 02-starting-project-deeper-mocking/posts/posts.test.js
@@ -0,0 +1,28 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { savePost, extractPostData } from './posts'
+
+const testTitle = 'Test title'
+const testContent = 'Test content'
+
+let testFormData
+
+
+describe('extractPostData()', () => {
+  beforeEach(() => {
+    testFormData = {
+      title: testTitle,
+      content: testContent,
+    
+      get(identifier) {
+        return this[identifier]
+      }
+    } 
+  })
+  it('should extract title and content from the provided form data', () => {
+    const data = extractPostData(testFormData)
+
+    expect(data.title).toBe(testTitle)
+    expect(data.content).toBe(testContent)
+  })
+
+})
