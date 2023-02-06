6d0a828769f50c957d5d91fcb6edef0b3efb0e3b
EliRodrEs <60432604+EliRodrEs@users.noreply.github.com>
2023-02-06 16:59:17 +0100

    Add DOM tests

---
+++ 02-starting-project-deeper-mocking/util/dom.test.js
@@ -0,0 +1,45 @@
+import { it, expect, vi, beforeEach } from 'vitest'
+import { showError } from './dom'
+import { Window } from 'happy-dom'
+import fs from 'fs'
+import path from 'path'
+
+const htmlDocPath = path.join(process.cwd(), 'index.html')
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString()
+const window = new Window()
+const document = window.document
+
+beforeEach(() => {
+  document.body.innerHTML = ''
+  document.write(htmlDocumentContent)
+})
+
+vi.stubGlobal('document', document)
+
+
+
+it('should add an error paragraph to the id="errors" element', () => {
+  showError('Test')
+
+  const errorsEl = document.getElementById('errors')
+  const errorParagraph = errorsEl.firstElementChild
+
+  expect(errorParagraph).not.toBeNull()
+})
+
+it('should not contain an error paragraph initially', () => {
+  const errorsEl = document.getElementById('errors')
+  const errorParagraph = errorsEl.firstElementChild
+
+  expect(errorParagraph).toBeNull()
+})
+
+it('should output the provided message in the error paragraph', () => {
+  const testErrorMessage = 'Test'
+  showError(testErrorMessage)
+
+  const errorsEl = document.getElementById('errors')
+  const errorParagraph = errorsEl.firstElementChild
+
+  expect(errorParagraph.textContent).toBe(testErrorMessage)
+})
